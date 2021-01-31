ed90d2a47f176e4daeed6bbbfa7b207d82dde8ce
Victor <VictorCodrean@users.noreply.github.com>
2021-01-30 17:34:27 -0800

    added more weather conditions and UV index as well

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for acity
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature,
+// the humidity, the wind speed, and the UV index.
 var searchBtnEl = document.querySelector(".search-btn");
-var sampleUrl = "api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}"
+var cityInfoEl = document.querySelector("#city-content")
 var apiKey = "a9e49bbfb982db505e4157a83863ddcc";
-var cityHeaderEl = document.querySelector("#city-content")
+var timeStampsCount = 4;
 
-function getWeatherData(cityNameInput) {
+function getWeatherData(cityNameInput, lat, lon) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameInput}&appid=${apiKey}&units=imperial`
+
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
@@ -14,19 +16,45 @@ function getWeatherData(cityNameInput) {
         .then(function (data) {
             console.log(data);
 
-            cityHeaderEl.setAttribute("class", "card");
-            var cityHeader = $("<h5>").addClass("card-header").text("Forecast Dashboard:");
+            cityInfoEl.setAttribute("class", "card");
+            var cityHeader = $("<h5>").addClass("card-header").text(data.name);
+            //  + data.weather[0].icon
             var cardBody = $("<div>").addClass("card-body");
-            var cardTitle = $("<h4>").addClass("card-title").text(data.name);
-            var cityTemp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " F")
+            var cardTitle = $("<h4>").addClass("card-title").text("Weather Conditions:");
+            var cityTemp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °F");
+            var cityHumidity = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+            var cityWindSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " miles/hour");
+
+            var lat = data.coord.lat;
+            var long = data.coord.lon;
+            uvIndex(lat, long, timeStampsCount, apiKey)
 
             $("#city-content").append(cityHeader);
             $("#city-content").append(cardBody);
             cardBody.append(cardTitle);
             cardBody.append(cityTemp);
-        })
+            cardBody.append(cityHumidity);
+            cardBody.append(cityWindSpeed);
+        });
+
 };
 
+function uvIndex(lat, long, timeStampsCount, apiKey) {
+    // got the UV index for 4 days "might include for 4 days forecast"...
+    var requestUrlUvIndex = `http://api.openweathermap.org/data/2.5/uvi/forecast?lat=${lat}&lon=${long}&cnt=${timeStampsCount}&appid=${apiKey}`
+    fetch(requestUrlUvIndex)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+            var cityUvIndex = $("<p>").addClass("card-text").text("UV index: " + data[0].value);
+
+            $("#city-content .card-body").append(cityUvIndex);
+
+        });
+};
 
 $(".search-btn").on("click", function () {
     var cityNameInput = $("#search-input").val();
@@ -35,17 +63,9 @@ $(".search-btn").on("click", function () {
 });
 
 
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
 
-
-// function check() {
-//     if (globalCityVal === "") {
-//         alert("it's empty")
-//     } else console.log(globalCityVal)
-
-// }
-
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
